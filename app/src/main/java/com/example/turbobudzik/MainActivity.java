4c71b7e54c45dc729e97cb575193bcbd621a61fe
Tomasz Wychocki <wychoniu@gmail.com>
2019-09-28 09:03:12 +0200

    Alarm

--- app/src/main/java/com/example/turbobudzik/MainActivity.java
+++ app/src/main/java/com/example/turbobudzik/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.turbobudzik;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
     private Database db;
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +33,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         db = new Database(this);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         showLessons();
     }
@@ -41,5 +51,34 @@ public class MainActivity extends AppCompatActivity
 
         List<Lesson> lessons = db.getLessonsBetween(dateFrom, dateTo);
         UserInterface.updateSchedule(lessons, this);
+    }
+
+    public void OnToggleClicked(View view)
+    {
+        long time;
+        if (((ToggleButton) view).isChecked())
+        {
+            Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 8);
+            calendar.set(Calendar.MINUTE, 59);
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+            time = (calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
+            if(System.currentTimeMillis() > time)
+            {
+                if (calendar.AM_PM == 0)
+                    time = time + (1000*60*60*12);
+                else
+                    time = time + (1000*60*60*24);
+            }
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+        }
+        else
+        {
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        }
     }
 }
